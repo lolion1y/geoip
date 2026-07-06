224dcd8af93cd4421f765c34a5fefd4e376f8595
lolion1y <93830081+lolion1y@users.noreply.github.com>
2026-07-06 16:43:52 +0800

    chore: add retry for fetching ASN

--- asn.sh
+++ asn.sh
@@ -16,8 +16,11 @@ while IFS= read -r line; do
     url="https://stat.ripe.net/data/ris-prefixes/data.json?list_prefixes=true&types=o&resource=${asn}"
     echo "-----------------------"
     echo "Fetching ${asn}..."
-    curl -sL ${url} -o ./tmp/${filename}-${asn}.txt \
+    curl -sSfL --retry 5 --retry-delay 2 --retry-all-errors ${url} -o ./tmp/${filename}-${asn}.txt \
       -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
+    echo "::group::${asn}"
+    cat "/tmp/${filename}-${asn}.txt"
+    echo "::endgroup::"
     jq --raw-output '.data.prefixes.v4.originating[]' ./tmp/${filename}-${asn}.txt | sort -u >>${file}
     jq --raw-output '.data.prefixes.v6.originating[]' ./tmp/${filename}-${asn}.txt | sort -u >>${file}
   done
