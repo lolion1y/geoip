3e380e87df11d36219f97017f5f9e6181e8c186b
lolion1y <93830081+lolion1y@users.noreply.github.com>
2026-07-06 16:57:51 +0800

    fix: ASN

--- asn.sh
+++ asn.sh
@@ -20,6 +20,7 @@ while IFS= read -r line; do
       -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
     echo "::group::${asn}"
     cat "./tmp/${filename}-${asn}.txt"
+    echo
     echo "::endgroup::"
     jq --raw-output '.data.prefixes.v4.originating[]' ./tmp/${filename}-${asn}.txt | sort -u >>${file}
     jq --raw-output '.data.prefixes.v6.originating[]' ./tmp/${filename}-${asn}.txt | sort -u >>${file}
